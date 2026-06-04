b3b3c4f88090b34e545c16e2d44be1b0679c7e7f
Bl4cky99 <info@bl4cky.de>
2026-06-04 12:59:00 +0200

    feat(statusline): adding buffer-line and maps for changing buffers

--- lua/config/statusline.lua
+++ lua/config/statusline.lua
@@ -19,5 +19,26 @@ require("lualine").setup({
 		lualine_y = { "progress" },
 		lualine_z = { "location" },
 	},
+	tabline = {
+		lualine_a = {
+			{
+				"buffers",
+				mode = 2,
+				show_filename_only = true,
+				show_modified_status = true,
+				symbols = {
+					modified = " ●",
+					alternate_file = "",
+					directory = "",
+				},
+				buffers_color = {
+					active = { fg = "#0A0A0A", bg = "#D4879C", gui = "bold" },
+					inactive = { fg = "#7f8c8d", bg = "#241A20" },
+				},
+				seperator = { left = "", right = "" },
+			},
+		},
+		lualine_z = { "tabs" },
+	},
 })
 
