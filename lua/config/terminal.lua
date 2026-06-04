073aec252afa70ac76b182c38400e875fc64624e
Bl4cky99 <info@bl4cky.de>
2026-06-04 13:33:22 +0200

    feat(terminal): now using remote terminal in remote sessions

--- lua/config/terminal.lua
+++ lua/config/terminal.lua
@@ -7,6 +7,53 @@ require("toggleterm").setup({
 	direction = "horizontal",
 })
 
+local Terminal = require("toggleterm.terminal").Terminal
+
+local remote_terms = {}
+local local_term = nil
+
+local function detect_remote_host()
+	local cwd = vim.fn.getcwd()
+	local sshfs_base = vim.fn.expand("$HOME/.sshfs/")
+
+	if not vim.startswith(cwd, sshfs_base) then
+		return nil
+	end
+
+	local rest = cwd:sub(#sshfs_base + 1)
+	return rest:match("^([^/]+)")
+end
+
+local function toggle_smart_terminal()
+	local host = detect_remote_host()
+
+	if host then
+		if not remote_terms[host] then
+			remote_terms[host] = Terminal:new({
+				cmd = "ssh " .. host,
+				direction = "horizontal",
+				hidden = true,
+				close_on_exit = false,
+				display_name = "ssh:" .. host,
+			})
+		end
+		remote_terms[host]:toggle()
+	else
+		if not local_term then
+			local_term = Terminal:new({
+				direction = "horizontal",
+				hidden = true,
+				display_name = "local",
+			})
+		end
+		local_term:toggle()
+	end
+end
+
+vim.keymap.set({ "n", "t" }, [[<C-\>]], toggle_smart_terminal, {
+	desc = "terminal: smart toggle (local/ssh)",
+})
+
 autocmd("TermOpen", {
 	pattern = "term://*toggleterm#*",
 	callback = function()
